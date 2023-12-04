3986e8379923e42c18707a66865a497214dd592e
Jérémy LEMAITRE <77986677+Je-Lemaitre@users.noreply.github.com>
2023-12-04 09:57:04 +0100

    2023-04-12_UI-GridSelection_Assembly

--- Hanjie_UI.py
+++ Hanjie_UI.py
@@ -1,32 +1,31 @@
-import customtkinter as ctk
-from PIL import Image
+import tkinter as tk
+from tkinter import ttk, messagebox
+from PIL import Image, ImageTk  # Import ImageTk from PIL
 from datetime import datetime
 import hanjie
+import grid_board_selection as gbs
+from os import listdir
+from os.path import join
+import random as rd
 
-ctk.set_default_color_theme("dark-blue")
 
-
-class HanjieHomePage(ctk.CTk):
-    """
-    Implement the Home Page of the game Hanjie in Custom Tkinter
-    """
+class HanjieHomePage(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Hanjie - Puzzle Game")
         self.geometry("800x600")
-        self.configure()
 
         # Header with Title and Image
+        game_image = Image.open("hanjie_image.png").resize((50, 50))
+        self.game_image_tk = ImageTk.PhotoImage(game_image)  # Use ImageTk to convert to Tkinter-compatible format
 
-        # Load and resize the game image
-        game_image = ctk.CTkImage(Image.open("hanjie_image.png") , size=(50,50))
-        header_frame = ctk.CTkFrame(self)
+        header_frame = tk.Frame(self)
         header_frame.pack(pady=20)
 
-        image_label = ctk.CTkLabel(header_frame, image=game_image)
+        image_label = tk.Label(header_frame, image=self.game_image_tk)  # Use self.game_image_tk here
         image_label.grid(row=0, column=0, padx=10)
 
-        title_label = ctk.CTkLabel(header_frame, text="Welcome to Hanjie - Puzzle Game", font=("Helvetica", 24))
+        title_label = tk.Label(header_frame, text="Welcome to Hanjie - Puzzle Game", font=("Helvetica", 24))
         title_label.grid(row=0, column=1, padx=20)
 
         # Game Description
@@ -35,39 +34,37 @@ class HanjieHomePage(ctk.CTk):
             "The goal is to reveal a hidden picture by painting the correct cells on a grid. "
             "Solve puzzles of varying difficulty and enjoy the challenge!"
         )
-        description_label = ctk.CTkLabel(self, text=description_text, font=("Helvetica", 14), wraplength=600)
+        description_label = tk.Label(self, text=description_text, font=("Helvetica", 14), wraplength=600)
         description_label.pack(pady=20)
 
         # Player Pseudonym Entry
-        pseudo_label = ctk.CTkLabel(self, text="Enter Your Pseudonym:", font=("Helvetica", 12))
+        pseudo_label = tk.Label(self, text="Enter Your Pseudonym:", font=("Helvetica", 12))
         pseudo_label.pack(pady=5)
 
-        self.pseudo_entry = ctk.CTkEntry(self, width=200)
+        self.pseudo_entry = tk.Entry(self, width=40)
         self.pseudo_entry.pack(pady=10)
 
         # Configure Game Button
-        start_button = ctk.CTkButton(self, text="Configure the Game", command=self.show_config_window, width=20, height=2)
+        start_button = tk.Button(self, text="Configure the Game", command=self.show_config_window, width=20, height=2)
         start_button.pack(pady=10)
 
         # Exit Game Button
-        exit_button = ctk.CTkButton(self, text="Exit Game", command=self.exit_game, width=20, height=2)
+        exit_button = tk.Button(self, text="Exit Game", command=self.exit_game, width=20, height=2)
         exit_button.pack(pady=10)
 
         # Rankings Section
-        last_results_label = ctk.CTkLabel(self, text="Rankings", font=("Helvetica", 16))
+        last_results_label = tk.Label(self, text="Rankings", font=("Helvetica", 16))
         last_results_label.pack(pady=10)
 
-        self.last_results_labels = []  # Store references to the labels displaying results
+        self.last_results_labels = []
 
-        # Store the last results in this list
         self.last_results = [
             {"date": "2023-01-01", "player": "Donald", "result": 650},
             {"date": "2023-01-02", "player": "Mickey", "result": 800},
             {"date": "2023-01-03", "player": "Felix", "result": 640},
-            # ... To be replaced by results stored in a csv or json file ?
+            # ... To be replaced by results stored in a csv or json file?
         ]
 
-        # Display initial results on the home page
         self.update_last_results()
 
     def show_config_window(self):
@@ -76,105 +73,105 @@ class HanjieHomePage(ctk.CTk):
         config_window.wait_window()
 
     def start_game(self, difficulty, grid_size, theme):
-        # Get the player's pseudonym
         player_name = self.pseudo_entry.get()
         if not player_name:
-            textbox = ctk.CTkTextbox(self)
-            textbox.configure(state="disabled")
-            textbox.insert("0.0","Error: Please enter your pseudonym before starting the game.")
-            print("Error: Please enter your pseudonym before starting the game.")
+            messagebox.showerror("Error", "Please enter your pseudonym before starting the game.")
             return
 
+        mypath = "pictures"
+        imgs = listdir(mypath)
+        if theme == "Travel":
+            img = "pictures/donald.png"
+        elif theme == "Cartoon":
+            img = "pictures/donald.png"
+        else:
+            img = join(mypath, rd.choice(imgs))
 
-        if theme == "random" :
-            img = "pictures/donald.png"
-            print("random")
-        if theme == "Cartoon" :
-            img = "pictures/donald.png"
-            print("Cartoon")
-        img = "pictures/donald.png"
         nbLines, nbColumns = grid_size.split("x")
         grid = (int(nbLines), int(nbColumns))
         hanjie.start_game(difficulty, grid, img)
-        # Add the code to launch the game with selected difficulty, grid size, and theme
-        # Here is a result, for example
-        result = 1000  # Replace with actual game result
+        result = 1000
         date = datetime.now().strftime("%Y-%m-%d %H:%M")
 
-        # Store the result
         self.last_results.append({"date": date, "player": player_name, "result": result})
-
-        # Update the last results labels
         self.update_last_results()
 
     def exit_game(self):
-        # The data may be saved before closing the window.
-        self.destroy()  # Close the main window
+        self.destroy()
 
     def update_last_results(self):
-        # Display the last 10 results on the home page
         for label in self.last_results_labels:
             label.destroy()
 
         self.last_results_labels = []
 
-        sorted_results = sorted(self.last_results, key=lambda res:res["result"], reverse=True)
+        sorted_results = sorted(self.last_results, key=lambda res: res["result"], reverse=True)
 
         for i, result in enumerate(sorted_results[-10:]):
-            result_label = ctk.CTkLabel(
-                self, text=f"{i + 1}. Player: {result['player']} \t Date: {result['date']} \t Result: {result['result']}"
+            result_label = tk.Label(
+                self,
+                text=f"{i + 1}. Player: {result['player']} \t Date: {result['date']} \t Result: {result['result']}"
             )
             result_label.pack(anchor="w", padx=20)
             self.last_results_labels.append(result_label)
 
 
-class GameConfigWindow(ctk.CTkToplevel):
+class GameConfigWindow(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
         self.title("Game Configuration")
-        self.geometry("400x300")
-        self.configure()
+        self.geometry("400x500")
 
-        # Difficulty Selection
-        difficulty_label = ctk.CTkLabel(self, text="Select Difficulty:", font=("Helvetica", 12))
+        difficulty_label = tk.Label(self, text="Select Difficulty:", font=("Helvetica", 12))
         difficulty_label.pack(pady=5)
 
         difficulty_options = ["Easy", "Medium", "Hard"]
-        self.difficulty_combobox = ctk.CTkComboBox(self, values=difficulty_options)
-        self.difficulty_combobox.set(difficulty_options[0])  # Default: Easy
+        self.difficulty_combobox = ttk.Combobox(self, values=difficulty_options)
+        self.difficulty_combobox.set(difficulty_options[0])
         self.difficulty_combobox.pack(pady=10)
 
-        # Grid Size Selection
-        grid_size_label = ctk.CTkLabel(self, text="Select Grid Size:", font=("Helvetica", 12))
+        grid_size_label = tk.Label(self, text="Select Grid Size:", font=("Helvetica", 12))
         grid_size_label.pack(pady=5)
 
-        self.grid_size_entry = ctk.CTkEntry(self)
+        self.grid_size_entry = tk.Entry(self)
         self.grid_size_entry.pack(pady=10)
-        self.grid_size_entry.insert(0,"10x10")
+        self.grid_size_entry.insert(0, "10x10")
+        grid_select_button = tk.Button(self, text="Grid Selection", command=self.selectGrid, width=20, height=2)
+        grid_select_button.pack(pady=10)
 
-        # Theme Selection
-        theme_label = ctk.CTkLabel(self, text="Select Theme:", font=("Helvetica", 12))
+        theme_label = tk.Label(self, text="Select Theme:", font=("Helvetica", 12))
         theme_label.pack(pady=5)
 
-        theme_options = ["Random","Cartoon", "Travel", "Animals"]  # Replace with your theme options
-        self.theme_combobox = ctk.CTkComboBox(self, values=theme_options)
-        self.theme_combobox.set(theme_options[0])  # Default: Random
+        theme_options = ["Random", "Cartoon", "Travel", "Animals"]
+        self.theme_combobox = ttk.Combobox(self, values=theme_options)
+        self.theme_combobox.set(theme_options[0])
         self.theme_combobox.pack(pady=10)
 
-        # Launch Game Button
-        launch_button = ctk.CTkButton(
+        launch_button = tk.Button(
             self, text="Launch Game", command=self.launch_game, width=20, height=2
         )
         launch_button.pack(pady=10)
 
+        exit_config_button = tk.Button(
+            self, text="Exit Configuration", command=self.exit_config, width=20, height=2
+        )
+        exit_config_button.pack(pady=10)
+
+    def selectGrid(self):
+        root = tk.Tk()
+        app = gbs.MyWindow(root)
+        root.mainloop()
+
+
     def launch_game(self):
-        # Get selected difficulty, grid size, and theme
         difficulty = self.difficulty_combobox.get()
         grid_size = self.grid_size_entry.get()
         theme = self.theme_combobox.get()
 
-        # Call the start_game method in the main window with selected options
         self.master.start_game(difficulty, grid_size, theme)
+        self.destroy()
+
+    def exit_config(self):
         self.destroy()
 
 
