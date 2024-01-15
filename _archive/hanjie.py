f1547bc1722aff82dd1eba6111e3bb8ddb838887
geayro <ronan.geay@ensta-bretagne.org>
2024-01-15 08:39:50 +0100

    update hanjie.py linked files

---
+++ _archive/hanjie.py
@@ -0,0 +1,20 @@
+from generateGrid import generateGrid
+from checkLabel import checkLabel
+from display import display
+from loadImage import loadImage
+from binarise import binarise
+
+if __name__ == "__main__":
+    #grid = generateGrid()
+
+    path = loadImage()
+    grid = binarise(path)
+    #grid = binarise('pictures/mickey.jpg')
+    #grid = binarise('pictures/zelda_1.jpg')
+    #grid = binarise('pictures/panda.webp')
+    #grid = binarise('pictures/EffeilTower.png')
+    #grid = binarise('pictures/taz_300_300.jpg')
+
+    labelsX, labelsY = checkLabel(grid)
+    #for row in grid: print(row)
+    display(grid, labelsX, labelsY, 1)
--- checkLabel.py
+++ checkLabel.py
@@ -1,27 +1,9 @@
+import itertools
+    
+def process_line(line):
+    return [sum(1 for _ in group) for key, group in itertools.groupby(line) if key == 'x'] or [0]
+
 def checkLabel(grid):
-
-    labelsX = []
-    labelsY = []
-
-    for y in range(len(grid)):
-        crossX = 0
-        labelX = []
-        for x in range(len(grid[0])):
-            crossX+=grid[y][x]=='x'
-            if crossX>0 and (grid[y][x]!='x' or x == len(grid[0])-1):
-                labelX.append(crossX)
-                crossX = 0
-        if(len(labelX)==0): labelX.append(0)
-        labelsX.append(labelX)
-
-    for x in range(len(grid[0])):
-        crossY = 0
-        labelY = []
-        for y in range(len(grid)):
-            crossY+=grid[y][x]=='x'
-            if crossY > 0 and (grid[y][x]!='x' or y == len(grid)-1):
-                labelY.append(crossY)
-                crossY = 0
-        if(len(labelY)==0): labelY.append(0)
-        labelsY.append(labelY)
+    labelsX = [process_line(row) for row in grid]
+    labelsY = [process_line(col) for col in zip(*grid)]
     return labelsX, labelsY
